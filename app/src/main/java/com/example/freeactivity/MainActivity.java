8184086adb3374ed6c7f1e2fe3304d1daceec976
mirandarcospin <mxr17e@acu.edu>
2020-10-12 18:21:03 -0500

    Initial commit. I did a view pager, it has only 3 pages, each one containing a bible verse

---
+++ app/src/main/java/com/example/freeactivity/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.freeactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.freeactivity.fragments.PageFragment1;
+import com.example.freeactivity.fragments.PageFragment2;
+import com.example.freeactivity.fragments.PageFragment3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<Fragment> list = new ArrayList<>();
+        list.add(new PageFragment1());
+        list.add(new PageFragment2());
+        list.add(new PageFragment3());
+
+        pager = findViewById(R.id.pager);
+        pagerAdapter = new SlidePageActivity(getSupportFragmentManager(),list);
+
+        pager.setAdapter(pagerAdapter);
+    }
+}
---
+++ app/src/main/java/com/example/freeactivity/fragments/PageFragment3.java
@@ -0,0 +1,23 @@
+package com.example.freeactivity.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.freeactivity.R;
+
+public class PageFragment3 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.page_3,container,false);
+
+        return rootView;
+    }
+}
